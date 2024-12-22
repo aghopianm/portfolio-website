0d1f4a1ba7af989b4ad947bafad50ab1a3081cc9
Minas Aghopian <aghopianm@gmail.com>
2024-12-22 21:26:22 +0100

    updates

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message, Mina will get back to you soon!');
-    this.reset();
+  e.preventDefault();  // Prevent the default form submission
+
+  // Create a FormData object from the form
+  const formData = new FormData(this);
+
+  // Send form data to Formspree using Fetch API
+  fetch('https://formspree.io/f/mdkkoolb', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      // Show the thank you message if the form was successfully submitted
+      alert('Thank you for your message, I will get back to you soon!');
+      this.reset();  // Reset the form
+    } else {
+      // Show an error message if submission fails
+      alert('Oops! Something went wrong. Please try again.');
+    }
+  })
+  .catch(error => {
+    // Show an error message if there's an issue with the request
+    alert('Oops! Something went wrong. Please try again.');
   });
-  
+});
